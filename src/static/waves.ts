97eeb2d4656353755f57cec47da9f46c12aa72ac
Luis Crespo <lcrespo@es.ibm.com>
2016-12-15 16:03:49 +0100

    Minor

--- src/static/waves.ts
+++ src/static/waves.ts
@@ -1,14 +1,14 @@
 import { Shape } from '../gamelib/shape';
-import { Route, Squadron } from '../wave';
+import { Route } from '../wave';
 
-let routeC = new Route(20, -20, 2, 1, [
-	{ steps: 36, ax: 0.2, ay: 0.1 },
-	{ steps: 100, ax: -0.2, ay: 0 }
-]);
-
-let routeD = new Route(460, -20, -2, 1, [
+let routeC = new Route(460, -20, -2, 1, [
 	{ steps: 36, ax: -0.2, ay: 0.1 },
 	{ steps: 100, ax: 0.2, ay: 0 }
+]);
+
+let routeD = new Route(20, -20, 2, 1, [
+	{ steps: 36, ax: 0.2, ay: 0.1 },
+	{ steps: 100, ax: -0.2, ay: 0 }
 ]);
 
 let routeU = new Route(20, -20, 2, 10, [
@@ -18,6 +18,16 @@ let routeU = new Route(20, -20, 2, 10, [
 let routeV = new Route(460, -20, -2, 10, [
 	{ steps: 1000, ax: 0, ay: -0.11 }
 ]);
+
+/* ToDo:
+	- Routes: N, inverted N, loop
+	- Random entry point for routes (within a range)
+	- Fully random routes (low priority)
+	- Random squadron selection
+	- Waves!
+	- Boss wave :-)
+	- Create harder version of existing route by multiplying speed & acceleration
+*/
 
 let enemyPoints = [
 	{ x: 0, y: 0 },
@@ -46,7 +56,7 @@ export default {
 	squadrons: [
 		{ route: routeU, shape: enemyShape3, ships: 5, steps: 20, delay: 120 },
 		{ route: routeV, shape: enemyShape2, ships: 5, steps: 20, delay: 240 },
-		{ route: routeC, shape: enemyShape1, ships: 5, steps: 20, delay: 370 },
-		{ route: routeD, shape: enemyShape2, ships: 5, steps: 20, delay: 470 }
+		{ route: routeD, shape: enemyShape1, ships: 5, steps: 20, delay: 370 },
+		{ route: routeC, shape: enemyShape2, ships: 5, steps: 20, delay: 470 }
 	]
 };
